adc0a31d9814d97e42c1b3d927809f979983fc8c
Yiğitcan Uçan <ucanyiit@gmail.com>
2018-07-31 13:24:35 +0300

    ErrorCode changes to sample app

--- app/src/main/java/com/drivebuddyapp/drivebuddysampleapp/DriveBuddyBootReceiver.java
+++ app/src/main/java/com/drivebuddyapp/drivebuddysampleapp/DriveBuddyBootReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.widget.Toast;
 
 import com.drivebuddyapp.drivebuddysdk.DriveBuddy;
+import com.drivebuddyapp.drivebuddysdk.DriveBuddyError;
 import com.drivebuddyapp.drivebuddysdk.DriveBuddyOperationalCallback;
 
 public class DriveBuddyBootReceiver extends BroadcastReceiver {
@@ -14,8 +15,8 @@ public class DriveBuddyBootReceiver extends BroadcastReceiver {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             final DriveBuddyOperationalCallback callback = new DriveBuddyOperationalCallback() {
                 @Override
-                public void onCompletion(boolean success, String message) {
-                    Toast.makeText(context, "DriveBuddy - " + message, Toast.LENGTH_SHORT).show();
+                public void onCompletion(boolean success, int errorCode) {
+                    Toast.makeText(context, "DriveBuddy - " + DriveBuddyError.getDefaultErrorMessage(errorCode), Toast.LENGTH_SHORT).show();
                 }
             };
             DriveBuddy.setup(context, DriveBuddy.getCurrentConfiguration(context), callback);
