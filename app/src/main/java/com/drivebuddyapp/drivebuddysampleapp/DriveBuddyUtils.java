95c259b379df5fc436d0253fe6b036a1dc11f991
Yiğitcan Uçan <ucanyiit@gmail.com>
2019-05-27 09:55:06 +0300

    Changes for Notification Provider

--- app/src/main/java/com/drivebuddyapp/drivebuddysampleapp/DriveBuddyUtils.java
+++ app/src/main/java/com/drivebuddyapp/drivebuddysampleapp/DriveBuddyUtils.java
@@ -8,24 +8,15 @@ import com.drivebuddyapp.drivebuddysdk.DriveBuddyNotification;
 import com.drivebuddyapp.drivebuddysdk.DriveBuddyOperationalCallback;
 
 public class DriveBuddyUtils {
-    static void setup(Context context, DriveBuddyOperationalCallback callback){
+    static void setup(Context context, DriveBuddyOperationalCallback callback) {
 
-        DriveBuddyNotification notification = new DriveBuddyNotification(
-                DriveBuddySharedPreferences.getString(context, "notificationTitle"),
-                DriveBuddySharedPreferences.getString(context, "notificationContent"),
-                DriveBuddySharedPreferences.getInt(context, "notificationIcon")
-        );
-
-        DriveBuddyConfiguration config = new DriveBuddyConfiguration(
-                DriveBuddySharedPreferences.getString(context, "sdk-key"),
+        DriveBuddyConfiguration config = new DriveBuddyConfiguration(DriveBuddySharedPreferences.getString(context, "sdk-key"),
                 DriveBuddySharedPreferences.getString(context, "username"),
                 DriveBuddySharedPreferences.getBoolean(context, "drivingDetection"),
-                notification,
                 DriveBuddySharedPreferences.getString(context, "firstName"),
                 DriveBuddySharedPreferences.getString(context, "surname"),
-                DriveBuddySharedPreferences.getString(context, "mail")
-        );
+                DriveBuddySharedPreferences.getString(context, "mail"));
 
-        DriveBuddy.setup(context, config, callback);
+        DriveBuddy.setup(context, config, callback, DriveBuddyNotificationProviderUtility.class);
     }
 }
